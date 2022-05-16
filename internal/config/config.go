3ca4fa87ea5f7ddb357166ff5fc22fe589057196
Dmitry Shalashov <skaurus@gmail.com>
2022-05-16 09:42:30 +0400

    increment 7 try 1

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,16 @@
 package config
 
 import (
-	"github.com/caarlos0/env/v6"
+	"flag"
 	"net/url"
+
+	"github.com/caarlos0/env/v6"
+)
+
+const (
+	DefaultServerAddr = "localhost:8080"
+	DefaultLogName    = "app.log"
+	DefaultBaseAddr   = "http://localhost:8080/"
 )
 
 type Config struct {
@@ -20,18 +28,37 @@ func ParseConfig() *Config {
 		panic(err)
 	}
 
+	var flagServerAddr, flagBaseAddr, flagStorageFileName string
+	flag.StringVar(&flagServerAddr, "a", DefaultServerAddr, "host:port to listen on")
+	flag.StringVar(&flagBaseAddr, "b", DefaultBaseAddr, "base addr for shortened urls")
+	flag.StringVar(&flagStorageFileName, "f", "", "filepath to store shortened urls")
+	flag.Parse()
+
 	if len(config.ServerAddr) == 0 {
-		config.ServerAddr = "localhost:8080"
+		if len(flagServerAddr) > 0 {
+			config.ServerAddr = flagServerAddr
+		} else {
+			config.ServerAddr = DefaultServerAddr
+		}
 	}
 	if len(config.LogName) == 0 {
-		config.LogName = "app.log"
+		config.LogName = DefaultLogName
 	}
 	if len(config.BaseAddr) == 0 {
-		config.BaseAddr = "http://localhost:8080/"
+		if len(flagBaseAddr) > 0 {
+			config.BaseAddr = flagBaseAddr
+		} else {
+			config.BaseAddr = DefaultBaseAddr
+		}
 	}
 	config.BaseURI, err = url.Parse(config.BaseAddr)
 	if err != nil {
 		panic(err)
+	}
+	if len(config.StorageFileName) == 0 {
+		if len(flagStorageFileName) > 0 {
+			config.StorageFileName = flagStorageFileName
+		}
 	}
 
 	return &config
