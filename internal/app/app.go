b83cf5f74449d8a80c07fae7ffb8a3a8250276cf
Dmitry Shalashov <skaurus@gmail.com>
2022-05-17 19:39:52 +0400

    increment 8 try 4

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	"compress/gzip"
+	"compress/zlib"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -53,28 +55,67 @@ func (w gzWriter) WriteHeader(statusCode int) {
 	w.ResponseWriter.WriteHeader(statusCode)
 }
 
+var gzipReader *gzip.Reader
+var gzipWriter *gzip.Writer
+
 func GzipCompression(c *gin.Context) {
-	if !strings.Contains(c.GetHeader("Accept-Encoding"), "gzip") {
-		c.Next()
-		return
-	}
-	switch c.GetHeader("Content-Type") {
-	case "application/json", "application/javascript", "text/plain", "text/html", "text/css", "text/xml":
-		gz, err := gzip.NewWriterLevel(c.Writer, gzip.BestCompression)
+	// разжимаем запрос
+	ce := c.GetHeader("Content-Encoding")
+	switch {
+	case ce == "gzip":
+		var err error
+		if gzipReader == nil {
+			gzipReader, err = gzip.NewReader(c.Request.Body)
+		} else {
+			err = gzipReader.Reset(c.Request.Body)
+		}
 		if err != nil {
 			c.String(http.StatusBadRequest, err.Error())
 			return
 		}
-		defer gz.Close()
-
-		c.Writer = gzWriter{c.Writer, gz}
-		c.Header("Content-Encoding", "gzip")
-		c.Next()
+		defer gzipReader.Close()
+		c.Request.Body = gzipReader
+	case ce == "deflate":
+		// в документации написано, что io.ReadCloser, который возвращает zlib.NewReader,
+		// имплементирует интерфейс Resetter с методом Reset - но кажется это не так :(
+		// а без Reset смысла в кешировании глобальной переменной нет
+		//err := zlibReader.Reset(c.Request.Body)
+		zlibReader, err := zlib.NewReader(c.Request.Body)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+		}
+		defer zlibReader.Close()
+		c.Request.Body = zlibReader
+	case len(ce) > 0:
+		c.String(http.StatusBadRequest, "unsupported Content-Encoding")
 		return
-	default:
+	}
+
+	if !strings.Contains(c.GetHeader("Accept-Encoding"), "gzip") {
 		c.Next()
 		return
 	}
+
+	// жмём ответ
+	switch c.GetHeader("Content-Type") {
+	case "application/json", "application/javascript", "text/plain", "text/html", "text/css", "text/xml":
+		if gzipWriter == nil {
+			var err error
+			gzipWriter, err = gzip.NewWriterLevel(c.Writer, gzip.BestCompression)
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+		} else {
+			gzipWriter.Reset(c.Writer)
+		}
+		defer gzipWriter.Close()
+
+		c.Writer = gzWriter{c.Writer, gzipWriter}
+		c.Header("Content-Encoding", "gzip")
+	}
+
+	c.Next()
 }
 
 func SetupRouter(storage *storage.Storage, config *config.Config) *gin.Engine {
